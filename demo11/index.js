8874e47658c5ce8fffb2d94a90aedccba101df10
lijun01@corp.netease.com <lijun01@corp.netease.com>
2016-12-18 22:08:11 +0800

    提交代码

--- demo11/index.js
+++ demo11/index.js
@@ -18,8 +18,8 @@ function WBGL(options){
             // ["air","http://test.nie.163.com/three_tech/resource/part1/airplane_1.png",512,512,150,100,-3000,1],
             // ["star1","http://test.nie.163.com/three_tech/resource/part1/star_1.png",2317,979,150,0,-4500,1],
             // ["dot1","../img/dot.png",40,40,750,-20,1000,1],
-            // ["dot2","../img/dot.png",40,40,-650,-20,1000,1],
-            ["light","../img/light.png",1152,1024,200,50,-7500,1]
+            ["play","../img/btn_video_play.png", 50, 50, 15, 200, 200, 1],
+            ["light","../img/light.png",1152, 1024, 200, 50, -7500, 1]
             // ["part1_bg","../img/part1_bg.jpg",2048*1.5,1024*1.5,0,0,-18000,1]
         ],
         haven: '',
@@ -30,6 +30,8 @@ function WBGL(options){
         isFocus: false,
         focusSet: '',
         sky: '',
+        videoEle: document.getElementById("myvideo"),
+        videoCanvas: '',
         dot: []
     };
     // _this = this;
@@ -86,11 +88,13 @@ WBGL.prototype = {
         
         self.addPlanes();
 
+        
+
         self.global.sky = self.addSkyBox(self.global.scene);
-        var obj1 = self.createSpriteMaterial("dot1", -400,20, 180, 60),
-            obj1_h = self.createSpriteMaterial("dot1_h", -400,20, 181, 0),
-            obj2 = self.createSpriteMaterial("dot2", 400,20, 180, 60),
-            obj2_h = self.createSpriteMaterial("dot2_h", 400,20, 181, 0);
+        var obj1 = self.createSpriteMaterial("dot1", -350,45, 380, 60),
+            obj1_h = self.createSpriteMaterial("dot1_h", -350,45, 381, 0),
+            obj2 = self.createSpriteMaterial("dot2", 350,40, 380, 60),
+            obj2_h = self.createSpriteMaterial("dot2_h", 350,40, 381, 0);
         self.global.dot.push(obj1);
         self.global.dot.push(obj1_h);
         obj1_h.visible = false;
@@ -102,11 +106,48 @@ WBGL.prototype = {
         self.global.scene.add(obj2);
         self.global.scene.add(obj2_h);
 
+        self.clickPointLoop(self.global.dot[0],[1,1.5]);
+        self.clickPointLoop(self.global.dot[1],[60,70]);
+        self.clickPointLoop(self.global.dot[3],[60,70]);
+        // new TWEEN.Tween( self.global.dot[0].scale )
+        //     .to({x:1.5,y:1.5,z:1.5}, 1500 )
+        //     .repeat(Infinity)
+        //     // .yoyo( true )
+        //     .easing( TWEEN.Easing.Cubic.InOut )
+        //     .start();
+
+        console.log(self.global.dot)
+        
         self.initLight();
 
         window.addEventListener( 'resize', function(){
             self.onresize();
         }, false );
+
+        self.createVideo('video',0,0,300,600, 400);
+    },
+    clickPointLoop:function (point,scale,time,delay){
+        var self = this;
+        console.log(self.global.isFocus)
+        if(self.global.isFocus){return}
+        time=time&&time!=0?time:1000;
+        delay=delay?delay:0;
+
+        if(point.scale.x<scale[1]){
+            new TWEEN.Tween(point.scale).to({x:scale[1],y:scale[1],z:scale[1]},time).delay(delay)
+                .onComplete(function(){
+                    self.clickPointLoop(point,scale);
+                })
+                .start();
+
+        }else{
+            new TWEEN.Tween(point.scale).to({x:scale[0],y:scale[0],z:scale[0]},time).delay(delay)
+                .onComplete(function(){
+                    self.clickPointLoop(point,scale);
+                })
+                .start();
+        }
+        
     },
     onresize: function(){
         var self = this;
@@ -157,6 +198,7 @@ WBGL.prototype = {
 
         self.global.planes.forEach(function(planeSet){
             var scale = (1500-planeSet[6])*2*Math.tan(22.5*Math.PI/180)/1080;
+            
             var plane = self.creatObj({
                 width:parseInt(planeSet[2]*scale),
                 height:parseInt(planeSet[3]*scale),
@@ -168,6 +210,10 @@ WBGL.prototype = {
             
             plane.name = planeSet[0];
             //plane.visible=false;
+            if(planeSet[0] == 'play'){
+                self.global.dot.push(plane);
+            }
+            
             self.global.scene.add(plane);
         })
     },
@@ -231,6 +277,7 @@ WBGL.prototype = {
             objPath:"haven/",
             objFileName:"threejs.obj",
             completeCallback:function(object){
+                $(".loading").text("100%");
                 object.traverse(function(child) { 
                     if (child instanceof THREE.Mesh) { 
                         child.material.side = THREE.DoubleSide;//设置贴图模式为双面贴图
@@ -251,16 +298,41 @@ WBGL.prototype = {
                 object.scale.y=0.01;//缩放级别
                 object.scale.z=0.01;//缩放级别
                 object.name="haven";//刚体名称
-                object.rotation.y=-Math.PI;//初始Y轴方向旋转角度
+                object.rotation.y=0;//初始Y轴方向旋转角度
                 self.global.haven = object;
                 self.global.scene.add(object);//添加到场景中
+                self.openAni();
                 self.animation();
             },
             progress:function(persent){
-                 
-                // $("#havenloading .progress").css("width",persent+"%");
+                $(".loading").text(persent+"%");
             }
         })
+    },
+    openAni: function(){
+        var self = this;
+        self.global.camera.lookAt( new THREE.Vector3(0,0,-20000) );
+        new TWEEN.Tween( self.global.haven.scale )
+            .to({x:1,y:1,z:1}, 500 )
+            .easing(TWEEN.Easing.Quadratic.InOut)
+            .delay(2000)
+            .start();
+        // self.global.haven.rotation.y += 0.05;
+        new TWEEN.Tween( self.global.haven.rotation )
+            .to({x:0,y:4.5,z:0}, 500 )
+            .easing(TWEEN.Easing.Quadratic.InOut)
+            .delay(2000)
+            .start();
+        $('.loading').hide();
+        if(self.global.isVR){
+            $('.pic1').show();
+            $('.pic2').show();
+            $('.pic3').hide();
+        }else{
+            $('.pic1').hide();
+            $('.pic2').hide();
+            $('.pic3').show();
+        }
     },
     addSkyBox: function(scene){
         var path = "../img/";
@@ -338,34 +410,67 @@ WBGL.prototype = {
         obj.name = name;
         return obj;
     },
+    createVideo: function(){
+        var self = this;
+        self.global.videoCanvas = document.createElement("canvas");
+        self.global.videoCanvas.width = w;
+        self.global.videoCanvas.height = h;
+        
+    },
+    playVideo: function(myvideo, canvas, w, h, x, y, z){
+        var self = this;
+        if (myvideo.paused) {
+            return;
+        }
+        canvas.getContext("2d").drawImage(myvideo,0,0,w,h);
+        var v = new THREE.SpriteMaterial( {
+            map: new THREE.CanvasTexture( canvas ),
+            blending: THREE.AdditiveBlending,
+            overdraw:false,
+            depthWrite:false
+        } )
+
+        obj = new THREE.Sprite(v);
+        obj.position.set( x, y, z );
+        self.global.scene.add(obj);
+    },
     inFocus: function(obj){
         var self = this;
         if(self.global.isFocus) {
             return;
         }
         self.global.isFocus = true;
-        // TweenMax.killAll();
-        TWEEN.removeAll();
-        // if(obj.name == 'dot1'){
-        //     TweenMax.to(self.global.dot[].scale,40,{x:10,y:10,z:10});
-        // }else{
-        //     TweenMax.to(obj.scale,40,{x:10,y:10,z:10});
-        // }
+        // var all = TWEEN.getAll();
         if(obj.name == 'dot1'){
-            self.global.dot[1].visible=true;
-            new TWEEN.Tween( self.global.dot[1].scale )
+            self.global.dot[2].visible=true;
+            new TWEEN.Tween( self.global.dot[2].scale )
                 .to({x:60,y:60,z:60}, 1500 )
+                .start();
+        }else if(obj.name == 'dot2'){
+            self.global.dot[4].visible=true;
+            new TWEEN.Tween( self.global.dot[4].scale )
+                .to({x:60,y:60,z:60}, 1500 )
+                .start();
+        }else if(obj.name == 'play'){
+            new TWEEN.Tween( obj.scale )
+                .to({x:2,y:2,z:2}, 1500 )
                 .start();
         }else{
-            self.global.dot[3].visible=true;
-            new TWEEN.Tween( self.global.dot[3].scale )
-                .to({x:60,y:60,z:60}, 1500 )
-                .start();
+            return;
         }
         
 
         self.global.focusSet = setTimeout(function(){
             console.log('in 1.5s');
+            if(obj.name == 'dot1'){
+                self.changeLookat(1, 5);
+            }else if(obj.name == 'dot2'){
+                self.changeLookat(1, 3.8);
+            }else if(obj.name == 'play'){
+                
+                self.global.videoEle.play();
+            }
+            
         },1500);
     },
     outFocus: function(){
@@ -374,39 +479,61 @@ WBGL.prototype = {
         if(!self.global.isFocus){return}
         self.global.isFocus = false;
         clearTimeout(self.global.focusSet);
-        // TweenMax.killAll();
-        // TweenMax.to(self.global.dot[1].scale,0,{x:0,y:0,z:0});
-        // TweenMax.to(self.global.dot[3].scale,0,{x:0,y:0,z:0});
-        TWEEN.removeAll();
-        new TWEEN.Tween( self.global.dot[1].scale )
+        // TWEEN.removeAll();
+        new TWEEN.Tween( self.global.dot[2].scale )
             .to({x:0,y:0,z:0}, 800 )
+            .onComplete(function(){
+                self.global.dot[2].visible = false;
+            })
             .start();
-        new TWEEN.Tween( self.global.dot[3].scale )
+        new TWEEN.Tween( self.global.dot[4].scale )
             .to({x:0,y:0,z:0}, 800 )
+            .onComplete(function(){
+                self.global.dot[4].visible = false;
+            })
             .start();
+        new TWEEN.Tween( self.global.dot[0].scale )
+            .to({x:1,y:1,z:1}, 800 )
+            .start();
+        self.changeLookat(0);
+        self.clickPointLoop(self.global.dot[0],[1,1.5]);
+        self.clickPointLoop(self.global.dot[1],[60,70]);
+        self.clickPointLoop(self.global.dot[3],[60,70]);
+    },
+    changeLookat: function(n, r_y){
+        var self = this;
+        if(n === 1){
+            new TWEEN.Tween( self.global.haven.scale )
+                .to({x:3,y:3,z:3}, 1000 )
+                .start();
+            new TWEEN.Tween( self.global.haven.rotation )
+                .to({x:0,y:r_y,z:0}, 1000 )
+                .start();
+        }else{
+            new TWEEN.Tween( self.global.haven.scale )
+                .to({x:1,y:1,z:1}, 800 )
+                .start();
+            new TWEEN.Tween( self.global.haven.rotation )
+                .to({x:0,y:4.5,z:0}, 800 )
+                .start();
+        }
+        
     },
     animation: function(){
         var self = this;
         
-        // self.global.camera.lookAt( new THREE.Vector3(0,0,-20000) );
-        if(self.global.haven.scale.z < 1){
-            self.global.haven.scale.z += 0.05;
-            self.global.haven.scale.y += 0.05;
-            self.global.haven.scale.x += 0.05;
-            self.global.haven.rotation.y += 0.05;
-        }else{
-            if(!self.global.isFocus){
-                // self.global.haven.rotation.y += 0.001;
-                // self.global.sky.mesh.rotation.y += 0.0005;
-                var newCameraPos=Math3D.getRotateAxis2d({
-                        x:self.global.camera.position.x,
-                        y:self.global.camera.position.z
-                    },-0.001,0);
-                    self.global.camera.position.x=newCameraPos.x;
-                    self.global.camera.position.z=newCameraPos.y;
-                
-                    self.global.camera.lookAt(self.global.scene.position);
-            }
+        self.playVideo(self.global.videoEle, self.global.videoCanvas, 600, 400, 0,0,400);
+        if(!self.global.isFocus){
+            // self.global.haven.rotation.y += 0.001;
+            // self.global.sky.mesh.rotation.y += 0.0005;
+            // var newCameraPos=Math3D.getRotateAxis2d({
+            //         x:self.global.camera.position.x,
+            //         y:self.global.camera.position.z
+            //     },-0.001,0);
+            //     self.global.camera.position.x=newCameraPos.x;
+            //     self.global.camera.position.z=newCameraPos.y;
+            
+            //     self.global.camera.lookAt(self.global.scene.position);
         }
         
         self.global.controls.update();
@@ -418,7 +545,7 @@ WBGL.prototype = {
         //碰撞检测
         self.global.raycaster.setFromCamera(self.global.mouse, self.global.camera);
         var intersects = self.global.raycaster.intersectObjects(self.global.dot);
-       
+
         if ( intersects.length > 0 && intersects[0].distance != 0) {
             // console.log(intersects);
             self.inFocus(intersects[0].object);
@@ -449,20 +576,11 @@ $btn.on('click', function(){
 
     if($this.index() == 1 && "onorientationchange" in window){
         isvr = true;
-        $('.pic1').show();
-        $('.pic2').show();
-        $('.pic3').hide();
     }else if($this.index() == 1){
         alert('您的设备不支持VR模式！');
-        $('.pic1').hide();
-        $('.pic2').hide();
-        $('.pic3').show();
-    }else if($this.index() == 0){
-        $('.pic1').hide();
-        $('.pic2').hide();
-        $('.pic3').show();
     }
     $this.parent().hide();
+    $('.loading').show();
     new WBGL({
         canvas: 'canvas-frame',
         isVR: isvr
